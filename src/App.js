9f442384fccdc93d971adb7493732488b54f660a
Felipe Morales Osorio <felipe.morales.osorio@gmail.com>
2022-06-20 14:21:05 -0400

    Deleting Functionality Added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import FeedbackData from './data/FeedbackData'
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure that you want to delete?')) {
+            // creates new array where the feedback filters out the items that don't have same id
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return (
         <> {/* This is called a fragment '<> </>', it is used to put in two things at once here */}
             
@@ -15,7 +22,7 @@ function App() {
             {/* Important: must use 'className' instead of 'class' for JSX (version of HTML used in React) */}
             {/*Important: this return must only have 1 html component*/}
             <div className='container'>
-                <FeedbackList feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
         </>
     )
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,21 @@
+import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
     return (
         // card class specified in index.css and does all styling
         <Card reverse={true}>
+            {/* Creates number display */}
             <div className="num-display">{item.rating}</div>
+            
+            {/* Creates button that is reactive (by passing in a callback function) */}
+            <button onClick={() => handleDelete(item.id)} className="close">
+                {/* Creates a purple X*/}
+                <FaTimes color='purple'/>
+            </button>
+
+            {/* Creates the text that will be displayed on Card */}
             <div className="text-display">{item.text}</div>
         </Card>
     )
